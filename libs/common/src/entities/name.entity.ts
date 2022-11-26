e87061feb7112a4deca669705db434e3bf2ffc97
Bennett Benedict <bennybenester@gmail.com>
2022-11-26 19:08:05 +0300

    Add name entity for all entities with name column

---
+++ libs/common/src/entities/name.entity.ts
@@ -0,0 +1,10 @@
+import { Column } from 'typeorm';
+import { DateEntity } from './date.entity';
+
+export class NameEntity extends DateEntity {
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  code: string;
+}
