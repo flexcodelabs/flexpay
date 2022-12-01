956420de23ebbb0ec700185a8175468ec2277434
Bennett Benedict <bennybenester@gmail.com>
2022-12-01 22:56:22 +0300

    Add unique between user and created channels

--- libs/common/src/entities/channel.entity.ts
+++ libs/common/src/entities/channel.entity.ts
@@ -1,9 +1,17 @@
-import { Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  Index,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+} from 'typeorm';
 import { Metadata } from '..';
 import { NameEntity } from './name.entity';
 import { User } from './user.entity';
 
 @Entity('channel', { schema: 'public' })
+@Index('unique_channel', ['name', 'createdBy.id'], { unique: true })
 export class Channel extends NameEntity {
   @ManyToOne(() => User, (user) => user.channels, {
     nullable: false,
