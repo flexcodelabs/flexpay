216d9902004403507cf5b9c2529d22df2a28a14d
Bennett Benedict <bennybenester@gmail.com>
2022-12-18 13:11:55 +0300

    Add core channel entity

---
+++ libs/common/src/entities/corechannel.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Index,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+} from 'typeorm';
+import { Metadata } from '..';
+import { NameEntity } from './name.entity';
+import { User } from './user.entity';
+
+@Entity('corechannel', { schema: 'public' })
+@Index('unique_prechannel', ['name', 'createdBy.id'], { unique: true })
+export class CoreChannel extends NameEntity {
+  @ManyToOne(() => User, (user) => user.channels, {
+    nullable: false,
+    cascade: false,
+    eager: false,
+  })
+  @JoinColumn({ name: 'userid', referencedColumnName: 'id' })
+  createdBy?: User;
+
+  @ManyToMany(() => Metadata, (metadata) => metadata, {
+    eager: false,
+    cascade: false,
+    nullable: true,
+  })
+  @JoinTable({
+    name: 'channelmetadata',
+    joinColumn: {
+      name: 'channel',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'metadata',
+      referencedColumnName: 'id',
+    },
+  })
+  metadata: Metadata[];
+}
