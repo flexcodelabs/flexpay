80a727d8f1a5c47cb9fa63a358534db932825c0e
Bennett Benedict <bennybenester@gmail.com>
2022-12-18 13:42:03 +0300

    Add relationship between metadata and core channel

--- libs/common/src/entities/metadata.entity.ts
+++ libs/common/src/entities/metadata.entity.ts
@@ -1,17 +1,25 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+} from 'typeorm';
+import { CoreChannel } from '..';
 import { NameEntity } from './name.entity';
 import { User } from './user.entity';
 
 @Entity('metadata', { schema: 'public' })
 export class Metadata extends NameEntity {
-  @Column({ unique: true })
-  value: string;
-
   @Column({ name: 'datatype' })
   dataType: string;
 
   @Column({ default: 'FALSE' })
   secret: boolean;
+
+  @Column({ default: 'FALSE' })
+  compulsory: boolean;
 
   @ManyToOne(() => User, (user) => user.metadata, {
     nullable: false,
@@ -20,4 +28,22 @@ export class Metadata extends NameEntity {
   })
   @JoinColumn({ name: 'userid', referencedColumnName: 'id' })
   createdBy?: User;
+
+  @ManyToMany(() => CoreChannel, (channel: any) => channel, {
+    eager: false,
+    cascade: false,
+    nullable: true,
+  })
+  @JoinTable({
+    name: 'channelmetadata',
+    joinColumn: {
+      name: 'metadata',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'channel',
+      referencedColumnName: 'id',
+    },
+  })
+  channels: CoreChannel[];
 }
